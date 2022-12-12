26d5f3803dd713f4c41a4a72f42e17bd5cad73e0
OnsFendri <onssfendri@yahoo.com>
2022-12-12 18:54:46 +0100

    nouveau commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* SLIDER */
 let img_Slider = document.getElementsByClassName('img_Slider');
 let etape = 0;
 let nbr__img = img_Slider.length;
@@ -7,46 +8,59 @@ let suivant = document.querySelector('.suivant');
 
 
 function enleverActiveImages() {
-    for(let i = 0 ; i < nbr__img ; i++) {
+    for (let i = 0; i < nbr__img; i++) {
         img_Slider[i].classList.remove('active');
     }
 }
 
-suivant.addEventListener('click', function() {
+suivant.addEventListener('click', function () {
     etape++;
-    if(etape >= nbr__img) {
+    if (etape >= nbr__img) {
         etape = 0;
     }
     enleverActiveImages();
     img_Slider[etape].classList.add('active');
 })
 
-precedent.addEventListener('click', function() {
+precedent.addEventListener('click', function () {
     etape--;
-    if(etape < 0) {
+    if (etape < 0) {
         etape = nbr__img - 1;
     }
     enleverActiveImages();
     img_Slider[etape].classList.add('active');
 })
 
-setInterval(function() {
+setInterval(function () {
     etape++;
-    if(etape >= nbr__img) {
+    if (etape >= nbr__img) {
         etape = 0;
     }
     enleverActiveImages();
     img_Slider[etape].classList.add('active');
 }, 3000)
 
-jQuery(function(){
+
+/* SCROLL UP */
+
+jQuery(function () {
     $(function () {
         $(window).scroll(function () { //Fonction appelée quand on descend la page
-            if ($(this).scrollTop() > 200 ) {  // Quand on est à 200pixels du haut de page,
-                $('#scrollUp').css('right','10px'); // Replace à 10pixels de la droite l'image
-            } else { 
-                $('#scrollUp').removeAttr( 'style' ); // Enlève les attributs CSS affectés par javascript pour cacher 
+            if ($(this).scrollTop() > 200) {  // Quand on est à 200pixels du haut de page,
+                $('#scrollUp').css('right', '10px'); // Replace à 10pixels de la droite l'image
+            } else {
+                $('#scrollUp').removeAttr('style'); // Enlève les attributs CSS affectés par javascript pour cacher 
             }
         });
     });
 });
+
+
+/* BARRE DE NAVIGATION RESPONSIVE */
+
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav_link');
+navLink.classList.toggle('hide');
+hamburger.addEventListener('click', () => {
+    navLink.classList.toggle('hide');
+})
